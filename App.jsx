04721d7d326e33c3fc4d939c145f8c0437b88e5c
Hsu Hlaing Hnin <sbd.mona@gmail.com>
2024-01-12 14:41:18 -0600

    update

--- App.jsx
+++ App.jsx
@@ -18,7 +18,12 @@ const App = () => {
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Button
         title="Calculate Inflation"
-        onPress={() => Analytics.trackEvent('calculate_inflation', {"Internet": "Wifi", "GPS":"Off"})}
+        onPress={() =>
+          Analytics.trackEvent('calculate_inflation', {
+            Internet: 'Wifi',
+            GPS: 'Off',
+          })
+        }
       />
     </View>
   );
