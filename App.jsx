8af2fc58e0331db9b55f2177857016cc4ff54a9c
Hsu Hlaing Hnin <sbd.mona@gmail.com>
2024-01-12 14:11:54 -0600

    add analytics event track

---
+++ App.jsx
@@ -0,0 +1,27 @@
+import React, {useEffect} from 'react';
+import {Alert, Button, View} from 'react-native';
+import Crashes from 'appcenter-crashes';
+import Analytics from 'appcenter-analytics';
+
+const App = () => {
+  useEffect(() => {
+    checkPreviousCrashes();
+  }, []);
+  const checkPreviousCrashes = async () => {
+    const didCrash = await Crashes.hasCrashedInLastSession();
+    if (didCrash) {
+      const report = await Crashes.lastSessionCrashReport();
+      Alert.alert('Sorry for the crash, we are working on a solution');
+    }
+  };
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Button
+        title="Calculate Inflation"
+        onPress={() => Analytics.trackEvent('calculate_inflation')}
+      />
+    </View>
+  );
+};
+
+export default App;
